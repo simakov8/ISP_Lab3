d6c8d335c26c564762badcf90c973484d9927ff7
simakov8 <egor.sima.2003@gmail.com>
2022-06-02 15:21:24 +0300

    changing SQLite to Postgress db

--- CulturalCherry/CulturalCherry/settings.py
+++ CulturalCherry/CulturalCherry/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'CulturalCherry.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'CulturalCherry',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
