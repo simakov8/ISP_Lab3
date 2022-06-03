559694e7db8cf3faf273043e9c1e76491f8c8c50
simakov8 <egor.sima.2003@gmail.com>
2022-06-03 21:09:27 +0300

    adding models

--- CulturalCherry/CCShop/models.py
+++ CulturalCherry/CCShop/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=50, verbose_name='Название')
+    foto = models.ImageField(upload_to='images/', verbose_name='Фото')
+    price = models.IntegerField(verbose_name='Цена')
+    sex = models.ForeignKey('Sex', on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+
+
+class Sex(models.Model):
+    sex = models.CharField(max_length=30, null=False, verbose_name='Пол')
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30, null=False, verbose_name='Название категории')
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, null=False)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=False)
+    count = models.IntegerField(verbose_name='Количество')
+
