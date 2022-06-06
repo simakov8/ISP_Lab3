e57a1120851e266a6d3978d98c868eaf67841460
simakov8 <egor.sima.2003@gmail.com>
2022-06-07 00:11:32 +0300

    adding filter to Catalog view

--- CulturalCherry/CCShop/views.py
+++ CulturalCherry/CCShop/views.py
@@ -13,3 +13,14 @@ class CatalogView(HeaderView, ListView):
     model = Product
     template_name = 'CCShop/catalog.html'
     context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'gender': Sex.objects.get(pk=self.kwargs['gender']),
+            'category': Category.objects.get(pk=self.kwargs['category']),
+        })
+        return context
+
+    def get_queryset(self):
+        return Product.objects.filter(category=self.kwargs['category'], sex=self.kwargs['gender'])
