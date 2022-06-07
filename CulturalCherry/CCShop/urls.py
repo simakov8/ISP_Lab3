e119d64f471268a09255f136be664725291cffff
simakov8 <egor.sima.2003@gmail.com>
2022-06-07 21:53:38 +0300

    adding authorization

--- CulturalCherry/CCShop/urls.py
+++ CulturalCherry/CCShop/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('catalog/<int:gender>/<int:category>/', CatalogView.as_view(), name='catalog'),
+    path('register/', SignUp.as_view(), name='register'),
+    path('login/', SignIn.as_view(), name='login'),
 ]
